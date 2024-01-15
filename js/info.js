ab7a8cba9e1d4972960550b0c655e4feb10ba8f3
ardhikaxx <ardhikayanuar58@gmail.com>
2024-01-15 22:28:09 +0700

    update

--- js/info.js
+++ js/info.js
@@ -6,13 +6,15 @@ buttons.forEach(button => {
 
         const iconSVG = project.querySelector('.icon-app').innerHTML;
         const paragraph = project.querySelector('.paragraf-info').textContent;
+        const year = project.querySelector('.year-info').textContent;
 
         Swal.fire({
             icon: 'info',
             iconColor: '#ce0000',
             title: 'Info',
             html: `<div class="icon-app">${iconSVG}</div>
-                    <p class="paragraf-info">${paragraph}</p>`,
+                    <p class="paragraf-info">${paragraph}</p>
+                    <h4 class="year-info">${year}</h4>`,
             confirmButtonColor: '#ce0000',
             confirmButtonText: 'Tutup',
             scrollbarPadding: false
