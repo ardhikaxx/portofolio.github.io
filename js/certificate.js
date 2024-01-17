937ab3710392573bcbea0b7493b08673ea9811f5
ardhikaxx <ardhikayanuar58@gmail.com>
2024-01-17 22:06:46 +0700

    update-

--- js/certificate.js
+++ js/certificate.js
@@ -8,15 +8,4 @@ document.addEventListener('DOMContentLoaded', function () {
         showCursor: true,
         CursorChar: "|"
     });
-
-    const sr = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 2000,
-        reset: true
-    });
-
-    sr.reveal('.btn-back', { delay: 100 });
-    sr.reveal('.text-center', { delay: 100 });
-    sr.reveal('.certificate-item', { delay: 200 });
 });
