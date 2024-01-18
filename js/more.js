4d812ce3110d6ec601b9bc3c137f829a8472bb60
ardhikaxx <ardhikayanuar58@gmail.com>
2024-01-18 12:47:31 +0700

    update

--- js/more.js
+++ js/more.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sr = ScrollReveal({
         origin: 'top',
-        distance: '80px',
         duration: 2000,
         reset: true
     });
