79d234b36b124cf4e561caa91f045a60d3ec391b
ardhikaxx <ardhikayanuar58@gmail.com>
2024-01-22 14:37:37 +0700

    update

--- js/gallery.js
+++ js/gallery.js
@@ -3,14 +3,17 @@ const buttons = document.querySelectorAll('.btn-gallery');
 buttons.forEach(button => {
     button.addEventListener('click', function () {
         const imageItem = this.closest('.image-item');
-
         const paragraph = imageItem.querySelector('.paragraf').textContent;
+        const imgSrc = this.getAttribute('data-img-src');
 
         Swal.fire({
             icon: 'info',
             iconColor: '#ce0000',
             title: 'Info',
-            html: `<p class="paragraf">${paragraph}</p>`,
+            html: `
+                <img loading="lazy" alt="" class="img-fluid rounded" src="${imgSrc}">
+                <p class="paragraf">${paragraph}</p>
+            `,
             confirmButtonColor: '#ce0000',
             confirmButtonText: 'Tutup',
             scrollbarPadding: false
