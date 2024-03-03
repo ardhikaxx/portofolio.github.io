16f87ed6c9c7f54f20248ab6e4a1594263222ed0
medinfo24 <ardhikayanuar58@gmail.com>
2024-03-03 12:22:16 +0700

    update

--- js/more.js
+++ js/more.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sr = ScrollReveal({
         origin: 'top',
-        distance: '80px',
+        distance: '50px',
         duration: 2000,
         reset: true
     });
 
     sr.reveal('.btn-back', { delay: 100 });
     sr.reveal('.more-header', { delay: 100 });
-    sr.reveal('.btn-gallery', { delay: 200 });
-    sr.reveal('.btn-cvAts', { delay: 200 });
-    sr.reveal('.btn-cvKreatif', { delay: 200 });
 });
